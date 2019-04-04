c8f2e5291a302493bb5e9fde882e8415fdc3eef3
Nicholas Moreno <nicholaslonggeno.moreno@calbaptist.edu>
2019-04-03 23:22:32 -0700

    Made the ability for the user to be able to continue as long as they want to use the program

--- src/TriangleInput.java
+++ src/TriangleInput.java
@@ -4,16 +4,18 @@ public class TriangleInput
 {
 
     public static void main(String args[]) {
-            String sA = "";
-            String sB = "";
-            String sC = "";
-            double test;
+        String sA = "";
+        String sB = "";
+        String sC = "";
+        double test;
+        String userAnswer = "";
 
-            boolean sAisDouble = false;
-            boolean sBisDouble = false;
-            boolean sCisDouble = false;
+        boolean sAisDouble = false;
+        boolean sBisDouble = false;
+        boolean sCisDouble = false;
 
-            
+        do
+            {
             System.out.println("Please enter 3 number lengths for each side of the" +
                     " triangle. Press enter after each number.");
             while (sAisDouble == false) {
@@ -21,7 +23,7 @@ public class TriangleInput
                 sA = sideA.nextLine();
                 try {
                     test = Double.valueOf(sA);
-                    if(test <= 0) {
+                    if (test <= 0) {
                         throw new NumberFormatException();
                     }
                     System.out.println("Side A: " + (sA));
@@ -37,45 +39,45 @@ public class TriangleInput
 
 
             //Moving on to sideB
-        while (sBisDouble == false) {
-            Scanner sideB = new Scanner(System.in);
-            sB = sideB.nextLine();
-            try {
-                test = Double.valueOf(sB);
-                if(test <= 0) {
-                    throw new NumberFormatException();
+            while (sBisDouble == false) {
+                Scanner sideB = new Scanner(System.in);
+                sB = sideB.nextLine();
+                try {
+                    test = Double.valueOf(sB);
+                    if (test <= 0) {
+                        throw new NumberFormatException();
+                    }
+                    System.out.println("Side B: " + (sB));
+                    break;
+                } catch (NumberFormatException bad) {
+                    System.out.println("ERROR! Non-numerical value has been entered. (Side B)");
+                    System.out.println("Please Enter a valid number");
                 }
-                System.out.println("Side B: " + (sB));
-                break;
-            } catch (NumberFormatException bad) {
-                System.out.println("ERROR! Non-numerical value has been entered. (Side B)");
-                System.out.println("Please Enter a valid number");
             }
-        }
 
-        //If the user successfully puts a integer / double, then proceed off the while loop
-        sBisDouble = true;
+            //If the user successfully puts a integer / double, then proceed off the while loop
+            sBisDouble = true;
 
 
             //Moving on to side C
-        while (sCisDouble == false) {
-            Scanner sideC = new Scanner(System.in);
-            sC = sideC.nextLine();
-            try {
-                test = Double.valueOf(sC);
-                if(test <= 0) {
-                    throw new NumberFormatException();
+            while (sCisDouble == false) {
+                Scanner sideC = new Scanner(System.in);
+                sC = sideC.nextLine();
+                try {
+                    test = Double.valueOf(sC);
+                    if (test <= 0) {
+                        throw new NumberFormatException();
+                    }
+                    System.out.println("Side C: " + (sC));
+                    break;
+                } catch (NumberFormatException bad) {
+                    System.out.println("ERROR! Non-numerical value has been entered. (Side C)");
+                    System.out.println("Please Enter a valid number");
                 }
-                System.out.println("Side C: " + (sC));
-                break;
-            } catch (NumberFormatException bad) {
-                System.out.println("ERROR! Non-numerical value has been entered. (Side C)");
-                System.out.println("Please Enter a valid number");
             }
-        }
 
-        //If the user successfully puts a integer / double, then proceed off the while loop
-        sCisDouble = true;
+            //If the user successfully puts a integer / double, then proceed off the while loop
+            sCisDouble = true;
 
 
             double s1 = Double.valueOf(sA);
@@ -91,20 +93,23 @@ public class TriangleInput
             //**********************************************************************
             //*********Now determining which type of triangle*************************
 
-                Triangle triangle = new Triangle(s1, s2, s3);
-                boolean canContinue = triangle.isNotTriangle(); //Checks to see if it is a valid triangle
+            Triangle triangle = new Triangle(s1, s2, s3);
+            boolean canContinue = triangle.isNotTriangle(); //Checks to see if it is a valid triangle
 
 
-                //If the user enters in a valid triangle, then see what it does here
-                if(canContinue == false) {
-                    triangle.isEquilateral();
-                    triangle.isScalene();
-                    triangle.isIsosceles();
-                }
+            //If the user enters in a valid triangle, then see what it does here
+            if (canContinue == false) {
+                triangle.isEquilateral();
+                triangle.isScalene();
+                triangle.isIsosceles();
             }
-            //Prompting the user about if they can continue or not
+            System.out.println("Would you like to continue? (Y/N)");
+            Scanner userinput = new Scanner(System.in);
+            userAnswer = userinput.next();
 
-            //Checks for the input of the user
+        } while (userAnswer.equals("Y") || userAnswer.equals("y"));
+    }
+
 
 
         }
